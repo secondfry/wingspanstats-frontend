6f193b3aa1468e56497a869c5e9d9871f2066300
Rustam Second_Fry Gubaydullin <secondfry@gmail.com>
2019-05-16 00:03:45 +0300

    node.js module update galore

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,71 @@
+// Old and probably working
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const glob = require('glob')
+const glob = require('glob');
 
-const extractScss = new ExtractTextPlugin('css/vue.css');
-
-module.exports = {
+const oldexports = {
   mode: process.env.NODE_ENV,
   entry: {
     app: './src/js/app.js',
   },
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dest')
+  },
+  externals: {
+    axios: 'axios',
+    localforage: 'localforage',
+    moment: 'moment',
+    vue: 'Vue',
+    vuex: 'Vuex',
+    'vue-router': 'VueRouter'
+  }
+};
+
+// Updated and confirmed working
+// Imports
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// Constants
+const extractVueSCSS = new MiniCssExtractPlugin({
+  filename: 'css/vue.css'
+});
+
+// Data
+newexports = {
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        'dest/css/vue*',
+        'dest/images',
+        'dest/js',
+      ]
+    }),
+    new VueLoaderPlugin(),
+    extractVueSCSS
+  ],
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          extractCSS: true,
-          loaders: {
-            scss: extractScss.extract(['css-loader', 'sass-loader', {
-              loader: 'sass-resources-loader',
-              options: {
-                resources: path.resolve(__dirname, './src/sass/_inject.scss')
-              }
-            }]),
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'vue-style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: path.resolve(__dirname, './src/sass/_inject.scss')
+            }
           }
-        }
+        ]
       },
       {
         test: /\.(png|svg)$/,
@@ -35,7 +75,7 @@ module.exports = {
             options: {
               name: '[name].[ext]?[hash]',
               outputPath: '/images/'
-            }  
+            }
           }
         ]
       },
@@ -52,25 +92,13 @@ module.exports = {
         ]
       }
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin([
-      'dest/css/vue*',
-      'dest/images',
-      'dest/js',
-    ]),
-    extractScss
-  ],
-  output: {
-    filename: 'js/[name].bundle.js',
-    path: path.resolve(__dirname, 'dest')
-  },
-  externals: {
-    axios: 'axios',
-    localforage: 'localforage',
-    moment: 'moment',
-    vue: 'Vue',
-    vuex: 'Vuex',
-    'vue-router': 'VueRouter'
   }
 };
+
+result = {};
+Object.assign(result, oldexports);
+Object.assign(result, newexports);
+
+console.log(result);
+
+module.exports = result;

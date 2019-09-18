5f8158e9f52bef0aeac1108c0502ac6414a3c9b5
Rustam Second_Fry Gubaydullin <secondfry@gmail.com>
2019-09-18 17:11:29 +0300

    Small attempts at minimizing DOM node spam

--- src/js/layouts/default.vue
+++ src/js/layouts/default.vue
@@ -59,31 +59,21 @@
       </div>
     </nav>
     <div class="container my-3">
-      <div class="row">
-        <div class="col">
-          <view-all-time></view-all-time>
-          <slot></slot>
-        </div>
-      </div>
+      <view-all-time></view-all-time>
+      <slot></slot>
     </div>
     <div class="p-3 my-3 text-center bg-success text-light">We are back!</div>
-    <footer class="sf-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col text-center">
-            <p>
-              Yours truly, <a href="https://twitter.com/Second_Fry">@Second_Fry</a> (<a
-                href="https://zkillboard.com/character/91435934/">Lenai Chelien</a>).
-              Adapted for web by in January, 2016.<br>
-              Rewritten in March, 2016.<br>
-              Rewritten in March, 2018. Consistency!<br>
-              Also check <a target="_blank" href="https://www.youtube.com/user/SecondFry">my YouTube channel</a> :P.
-            </p>
-            <p>Original idea by <a href="https://zkillboard.com/character/92805979/">Valtyr Farshield</a> @ <a
-                href="https://github.com/farshield/wingspanstats">GitHub</a>.</p>
-          </div>
-        </div>
-      </div>
+    <footer class="sf-footer container text-center">
+      <p>
+        Yours truly, <a href="https://twitter.com/Second_Fry">@Second_Fry</a> (<a
+          href="https://zkillboard.com/character/91435934/">Lenai Chelien</a>).
+        Adapted for web by in January, 2016.<br>
+        Rewritten in March, 2016.<br>
+        Rewritten in March, 2018. Consistency!<br>
+        Also check <a target="_blank" href="https://www.youtube.com/user/SecondFry" rel="noopener">my YouTube channel</a> :P.
+      </p>
+      <p>Original idea by <a href="https://zkillboard.com/character/92805979/">Valtyr Farshield</a> @ <a
+          href="https://github.com/farshield/wingspanstats">GitHub</a>.</p>
     </footer>
   </div>
 </template>
